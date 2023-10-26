8fdb3273982f91991dcc442bd1fb3ee794bb2a97
Hugues Prud'homme <70539777+Huguesmmm@users.noreply.github.com>
2023-10-25 22:21:08 -0400

    make the blob move

--- src/Blob.tsx
+++ src/Blob.tsx
@@ -1,7 +1,31 @@
 import Spline from '@splinetool/react-spline';
+import { useRef } from 'react';
+import './Blob.css'
 
 export default function Blob() {
+    const transblob = useRef<THREE.Object3D | null>(null);
+
+    function onload(spline: any){
+        const blob = spline.findObjectById('8cb38db9-0d1b-42af-8ac7-8d930026c83c');
+
+        transblob.current = blob;
+    }
+
+    function moveBlob(){
+        if (transblob.current) {
+            console.log(transblob.current);
+
+            transblob.current.position.x += 10;
+        }
+    }
+
     return (
-        <Spline scene="https://prod.spline.design/BKzVCK3es3jwln2y/scene.splinecode" />
+        <div>
+            <Spline
+                scene="https://prod.spline.design/BKzVCK3es3jwln2y/scene.splinecode"
+                onLoad={onload}
+            />
+            <button onClick={moveBlob}>Move Blob</button>
+        </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import Blob from './Blob.tsx'
 import './index.css'
 
